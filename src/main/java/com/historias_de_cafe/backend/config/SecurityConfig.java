7abed4fc4aedf6a806f2dff604edf667e2a80250
zullytamayom <zullytamayom@gmail.com>
2026-05-26 16:56:38 -0500

    ajuste acceso rutas para pagos

--- src/main/java/com/historias_de_cafe/backend/config/SecurityConfig.java
+++ src/main/java/com/historias_de_cafe/backend/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
